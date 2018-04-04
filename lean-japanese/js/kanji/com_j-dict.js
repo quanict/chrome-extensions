17b700b524da1ed00b232fb008f5429d8f5d7d16
quanict <hongquan2712@gmail.com>
2018-04-05 02:36:40 +0700

    update

--- lean-japanese/js/kanji/com_j-dict.js
+++ lean-japanese/js/kanji/com_j-dict.js
@@ -1,58 +1,83 @@
-function getNodesText(tagName){
+let ajaxLoading = false;
+function postICTJapan(data){
+	//console.log("send data",JSON.stringify(data) );
 
+    $( document ).ajaxStart(function() {
+        ajaxLoading = true;
+    });
+
+    $.ajaxSetup({
+        url: "http://jp.giaiphapict.loc/api/kanjiword",
+        global: false,
+        type: "POST",
+		crossDomain:true,
+        cache:true,
+        async:true,
+        success: function(msg){
+            console.log(msg);
+        },
+        error: function(jxhr){
+            console.log(jxhr);
+
+        }
+    });
+	if( !ajaxLoading ){
+        $.ajax({data: data});
+	}
 }
 
-Object.prototype.getNodesText = function(tagName) {
-	let text = [], that = this, tags = tagName;
-	if( typeof tagName === 'object' && tagName.length > 0 ){
-		let tagFirst = tags[0].toString(), tagsLeft = tagName.filter(item => item !== tagFirst);
-		if( tagsLeft.length > 0 ){
-			try {
-			    let tags = that.getElementsByTagName(tagFirst.toUpperCase());
-				if( tags.length > 0 ){
-					for (var tag of tags ) {
-						text.push( tag.getNodesText(tagsLeft) );
-					}
-				}
-			}
-			catch(err) {
-			    console.log("have error",{that,err});
-			}
-				
-		} else {
-			let nodeTexts = that.getNodesText(tagFirst);
-			text = [...text,...nodeTexts];
-		}
-	} else if ( typeof tagName === 'string') {
-		let texts = that.getElementsByTagName(tagName.toUpperCase());
-		if( texts.length > 0 ){
-			for (var w of texts ) {
-				text.push(w.innerText);
-			}
-		}
-	} else {
-		console.log("tagname line-42 :",{tagName,that});
-	}
-	return text;
-};
+
 
 function getKanjiChar(){
-	console.log("=======================begin getKanjiChar");
+    Node.prototype.getNodesText = function(tagName) {
+        let text = [], that = this, tags = tagName;
+        if( typeof tagName === 'object' && tagName.length > 0 ){
+            let tagFirst = tags[0].toString(), tagsLeft = tagName.filter(item => item !== tagFirst);
+            if( tagsLeft.length > 0 ){
+                try {
+                    let tags = that.getElementsByTagName(tagFirst.toUpperCase());
+                    if( tags.length > 0 ){
+                        for (let tag of tags ) {
+                            text.push( tag.getNodesText(tagsLeft) );
+                        }
+                    }
+                }
+                catch(err) {
+                    console.log("have error",{that,err});
+                }
+
+            } else {
+                let nodeTexts = that.getNodesText(tagFirst);
+                text = [...text,...nodeTexts];
+            }
+        } else if ( typeof tagName === 'string') {
+            let texts = that.getElementsByTagName(tagName.toUpperCase());
+            if( texts.length > 0 ){
+                for (var w of texts ) {
+                    text.push(w.innerText);
+                }
+            }
+        } else {
+            console.log("tagname line-42 :",{tagName,that});
+        }
+        return text;
+    };
+
 	let kanjiDetail = Array.from(document.querySelectorAll(".dekiru-popup-detail"));
 	if( kanjiDetail.length > 0 ){
 		kanjiDetail = kanjiDetail[0];
 		if( kanjiDetail.innerHTML.length > 0 ){
 			let data = {
-				kanji : document.querySelectorAll(".dekiru-popup-detail .qqq.japan-font")[0].innerText,
-				hanViet : document.querySelectorAll(".dekiru-popup-detail .qqe")[0].innerText,
-				svg : document.getElementById("drawkanji").innerHTML,
-				meaning:null, onyomi:null,kunyomi:null,
+                word : document.querySelectorAll(".dekiru-popup-detail .qqq.japan-font")[0].innerText,
+                chinese : document.querySelectorAll(".dekiru-popup-detail .qqe")[0].innerText,
+				//svg : document.getElementById("drawkanji").innerHTML,
+                vietnamese:null, onyomi:null,kunyomi:null,
 				parts:[],remembering:{image:null,explain:null}
 				//content : document.querySelectorAll(".dekiru-popup-detail .kanji-search-block")[0].innerHTML,
 			};
 
 			let searchData = Array.from(document.querySelectorAll(".dekiru-popup-detail .kanji-search-block"));
-			for (var i = 0; i < searchData.length; i++) {
+			for (let i = 0; i < searchData.length; i++) {
 				let testData = searchData[i];
 				let label = testData.getElementsByTagName("LABEL");
 				if( label.length > 0 && typeof label[0] !== 'undefined' ){
@@ -89,24 +114,24 @@ function getKanjiChar(){
 							if( searchValue.length > 0 ){
 								switch(label) {
 									case "Ý nghĩa":
-										data.meaning = searchValue[0].innerHTML;
+										data.vietnamese = searchValue[0].innerHTML;
 										break;
 									case "Giải thích":
-										data.explain = searchValue[0].innerHTML;
+										data.explanation = searchValue[0].innerHTML;
 										break;
 									case "Trình độ":
 										data.level = searchValue[0].innerText; break;
 									case "Số nét":
-										data.drawline = searchValue[0].innerText;break;
+										data.stroke = searchValue[0].innerText;break;
 									case "Cách ghi nhớ":
-										data.remembering.explain = searchValue[0].innerText;break;
+										data.remembering.explain = searchValue[0].innerText;
 										break;
 									default:
 										console.log("check data "+i+":",{label,testData});
 									break;
 								}
 							} else {
-								console.log("check data "+i+": value of '"+label+"'' is null",{searchValue,testData});
+								//console.log("check data "+i+": value of '"+label+"'' is null",{searchValue,testData});
 							}
 							break;
 					}
@@ -115,7 +140,7 @@ function getKanjiChar(){
 				}
 				
 			}
-			console.log("bug:",data); 		
+            postICTJapan(data);
 		}
 		
 	}
